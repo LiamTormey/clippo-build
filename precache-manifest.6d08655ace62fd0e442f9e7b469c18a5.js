e42f1476ba4cc184b22f044dd15ea6c51fddf7a4
Liam Tormey <lltsamoa@gmail.com>
2020-02-06 09:03:33 +1100

    Add files via upload

---
+++ precache-manifest.6d08655ace62fd0e442f9e7b469c18a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce87600f800d1ea6becaa055f5276c2",
+    "url": "/clippo-build/index.html"
+  },
+  {
+    "revision": "ce7678eb0839489b1724",
+    "url": "/clippo-build/static/css/main.e6b76432.chunk.css"
+  },
+  {
+    "revision": "0c2d261a90f528ee1158",
+    "url": "/clippo-build/static/js/2.db9758ea.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clippo-build/static/js/2.db9758ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7678eb0839489b1724",
+    "url": "/clippo-build/static/js/main.2b56dba8.chunk.js"
+  },
+  {
+    "revision": "997993e822d47eab52d1",
+    "url": "/clippo-build/static/js/runtime-main.f6754a92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clippo-build/static/media/logo.5d5d9eef.svg"
+  }
+]);
